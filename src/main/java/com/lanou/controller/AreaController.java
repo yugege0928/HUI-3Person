7af39a3ac853b7152e0424d82560c0eb827d1633
yugege0928 <2964036652@qq.com>
2017-11-08 19:35:28 +0800

    项目初建:测试分支3

--- src/main/java/com/lanou/controller/AreaController.java
+++ src/main/java/com/lanou/controller/AreaController.java
@@ -31,6 +31,9 @@ public class AreaController {
         System.out.println(5);
         System.out.println(6);
 
+        System.out.println(7);
+        System.out.println(8);
+
         return service.findAllArea();
     }
 
